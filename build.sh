b8008abae6cef48009628f13812b7226bf61173e
rdzzf8nn4g-gif <rdzzf8nn4g@privaterelay.appleid.com>
2026-06-11 16:00:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # 终端执行: chmod +x build.sh
 
+echo ">>> 正在检查并赋予维护者脚本执行权限..."
+# 加上 || true 是为了防止万一哪天你删了这两个文件，脚本也不会报错中断
+chmod +x postinst postrm || true
+
 echo ">>> 清理旧文件..."
 make clean
 
